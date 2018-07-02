f906d97fb195cf73998650d5d73f89c0c37ed06d
Yvon Nchonon <ynchonon@emoneyadvisor.com>
2018-07-02 17:57:03 -0400

    Add statements to Hacker.cs

---
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello Console");
+		Terminal.WriteLine("What would you like to hack into?");
+		Terminal.WriteLine("Press 1 for the local library");
+		Terminal.WriteLine("Press 2 for the Police Station");
+		Terminal.WriteLine("Enter you Selection: ");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
