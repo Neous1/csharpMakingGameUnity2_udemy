9cec2b9d8882ff39e82a3e488485246ab7e5255f
Yvon Nchonon <ynchonon@emoneyadvisor.com>
2018-07-03 05:23:03 -0400

    Add OnUserInput method, Print user's selection to console

--- 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Hacker : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        print("Hello Console");
-		Terminal.WriteLine("What would you like to hack into?");
-		Terminal.WriteLine("Press 1 for the local library");
-		Terminal.WriteLine("Press 2 for the Police Station");
-		Terminal.WriteLine("Enter you Selection: ");
+	void Start ()
+	{
+        ShowMainMenu("Yvon");
+	}
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void ShowMainMenu(string name)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello "+ name);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the Police Station");
+        Terminal.WriteLine("Enter you Selection: ");
+    }
+    void OnUserInput(string input)
+    {
+        print(input =="1");
+    }
+
 }
